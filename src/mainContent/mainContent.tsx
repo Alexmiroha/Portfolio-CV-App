72970322ecffb395acf6838125617c35cb27df0a
AlexPC <pssxopatik673@gmail.com>
2023-05-07 19:16:11 +0200

    was added gh-pages package

---
+++ src/mainContent/mainContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './mainContent.module.css'
+
+const MainContent = () => {
+    return (
+        <div className={s.mainContent}>
+            Main
+        </div>
+    );
+};
+
+export default MainContent;
