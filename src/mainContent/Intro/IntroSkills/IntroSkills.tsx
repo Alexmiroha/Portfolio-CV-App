dc71434e902c0836dc3998fc5b0ac5dbe27d36f2
AlexPC <pssxopatik673@gmail.com>
2023-05-14 23:19:39 +0200

    was added techStack block

--- src/mainContent/Intro/IntroSkills/IntroSkills.tsx
+++ src/mainContent/Intro/IntroSkills/IntroSkills.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import s from './IntroSkills.module.css'
+import html from '../../../img/html.svg'
+import css from '../../../img/css.svg'
+import js from '../../../img/js.svg'
+import ts from '../../../img/typescript.svg'
+import react from '../../../img/logo192.png'
+import MUI from '../../../img/MUI.svg'
+
 
 const IntroSkills = () => {
     return (
         <div className={s.introSkills}>
-
+            <p className={s.description}>Tech Stack |</p>
+            <ul className={s.icons}>
+                <li>
+                    <img className={s.icon} src={html} alt="HTML"/>
+                </li>
+                <li>
+                    <img className={s.icon} src={css} alt="CSS"/>
+                </li>
+                <li>
+                    <img className={s.icon} src={js} alt="JS"/>
+                </li>
+                <li>
+                    <img className={s.icon} src={ts} alt="TS"/>
+                </li>
+                <li>
+                    <img className={s.icon} src={react} alt="React"/>
+                </li>
+                <li>
+                    <img className={s.icon} src={MUI} alt="Material UI"/>
+                </li>
+            </ul>
         </div>
     );
 };
