77cf6ccaeb877ccf18797593c146ac6912c0b5c7
AlexPC <pssxopatik673@gmail.com>
2023-05-14 22:20:06 +0200

    was added some discrb to Intro component

--- src/mainContent/Intro/Intro.tsx
+++ src/mainContent/Intro/Intro.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './Intro.module.css'
 import IntroMain from "./IntroMain/IntroMain";
+import IntroSkills from "./IntroSkills/IntroSkills";
 
 const Intro = () => {
     return (
         <div className={s.introBlock}>
             <IntroMain/>
+            <IntroSkills/>
         </div>
     );
 };
--- src/mainContent/Intro/IntroMain/IntroMain.tsx
+++ src/mainContent/Intro/IntroMain/IntroMain.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import s from './IntroMain.module.css'
+import {BiMap} from "react-icons/bi";
+import {BsGithub, BsLinkedin} from "react-icons/bs";
+import {GrInstagram} from "react-icons/gr";
+import avatar from '../../../img/photo.jpg'
+import icon from '../../../img/logo192.png'
 
 const IntroMain = () => {
     return (
         <div className={s.introMain}>
             <div className={s.text}>
-                <h1 className={s.title}>Front-End React Developer</h1>
+                <h1 className={s.title}>Front-End React Developer
+                    <img className={s.icon} src={icon} alt=""/>
+                </h1>
                 <p className={s.description}>
                     Hi, I'm Oleksii Miroshnichenko. A passionate Front-end React Developer
                     based in Warsaw, Poland.
+                    <BiMap color='#d261fb'/>
                 </p>
+                <div className={s.social}>
+                    <base target='_blank'/>
+                    <a href="https://www.linkedin.com/in/alex-m-60354b263/" className={s.link}>
+                        <BsLinkedin size='25px'/>
+                    </a>
+                    <a href="https://github.com/Alexmiroha" className={s.link}>
+                        <BsGithub size='25px'/>
+                    </a>
+                    <a href="https://www.instagram.com/a1exm1roha_dev/" className={s.link}>
+                        <GrInstagram size='25px'/>
+                    </a>
+                </div>
             </div>
-            <div className={s.image}>
-                image
+            <div className={s.imageContainer}>
+                <img className={s.image} src={avatar} alt="image"/>
             </div>
         </div>
     );
