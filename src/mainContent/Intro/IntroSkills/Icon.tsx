b8a7167d30173cacd4a2b0a04df022c88bab95ae
AlexPC <pssxopatik673@gmail.com>
2023-05-24 23:10:04 +0200

    was added animations to icons

--- src/mainContent/Intro/IntroSkills/Icon.tsx
+++ src/mainContent/Intro/IntroSkills/Icon.tsx
@@ -6,8 +6,16 @@ export type IconPropsType = {
 }
 export const Icon: React.FC<IconPropsType> = (props) => {
     return (
-        <li className={s.icon}>
-            <img className={s.logo} src={props.src} alt="HTML"/>
-        </li>
+        <div className={s.flipCard}>
+            <div className={s.flipCardInner}>
+                <li className={s.flipCardFront}>
+                    <img className={s.logo} src={props.src} alt="HTML" />
+                </li>
+                <li className={s.flipCardBack}>
+                    <img className={s.logo} src={props.src} alt="HTML" />
+                </li>
+            </div>
+        </div>
+
     );
 };
