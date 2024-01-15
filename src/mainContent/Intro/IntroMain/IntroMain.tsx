5e9c8f5031c6e8a2973b311ba29f6dbe7b12ec7b
AlexPC <pssxopatik673@gmail.com>
2024-01-15 17:13:33 +0100

    was added some shadow [IntroComponent]

--- src/mainContent/Intro/IntroMain/IntroMain.tsx
+++ src/mainContent/Intro/IntroMain/IntroMain.tsx
@@ -11,7 +11,7 @@ type IntroMainPropsType = {
 
 const IntroMain = (props: IntroMainPropsType) => {
     return (
-        <div className={`${s.introMain} ${props.isDarkMode ? s._darkModeBC : ''}`}>
+        <div className={`${s.introMain} ${props.isDarkMode ? `${s._darkModeBC} ${s._darkMode}` : ''}`}>
             <div className={s.text}>
                 <h1 className={s.title}>Front-End React <br/>Developer
                     <img className={s.icon} src={icon} alt="icon React"/>
@@ -22,8 +22,8 @@ const IntroMain = (props: IntroMainPropsType) => {
                     <BiMap color='#d261fb'/>
                 </p>
             </div>
-            <div className={`${s.imageContainer} ${props.isDarkMode ? s._darkModeBC : ''}`}>
-                <img className={`${s.image} ${props.isDarkMode ? s._darkModeBC : ''}`} src={avatar} alt="avatar"/>
+            <div className={`${s.imageContainer} ${props.isDarkMode ? `${s._darkModeBC} ${s._darkMode}` : ''}`}>
+                <img className={`${s.image} ${props.isDarkMode ? `${s._darkModeBC} ${s._darkMode}` : ''}`} src={avatar} alt="avatar"/>
                 <Social display={["instagram", "github", "linkedin"]} iconHoverColor={'#d261fb'} iconColor={props.isDarkMode? '#fff' : ''}/>
 
             </div>
